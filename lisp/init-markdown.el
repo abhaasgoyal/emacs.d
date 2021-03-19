d0b55a1baaa0a06bdee5f46d0c4d3be98fc03cd9
abhaasgoyal <u7145384@anu.edu.au>
2021-03-19 06:21:44 +0530

    Display github rendered html on page

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -5,7 +5,10 @@
 (when (maybe-require-package 'markdown-mode)
   (add-auto-mode 'markdown-mode "\\.md\\.html\\'")
   (with-eval-after-load 'whitespace-cleanup-mode
-    (add-to-list 'whitespace-cleanup-mode-ignore-modes 'markdown-mode)))
+    (add-to-list 'whitespace-cleanup-mode-ignore-modes 'markdown-mode))
+  (when (maybe-require-package 'grip-mode)
+    (with-eval-after-load 'markdown-mode
+      (define-key markdown-mode-command-map (kbd "g") #'grip-mode))))
 
 
 (provide 'init-markdown)
